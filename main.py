aaecfe649829b7a53309bec8cf5732196d1ec3ce
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-20 17:27:31 +0700

    learn sekuensial

--- main.py
+++ main.py
@@ -3,4 +3,10 @@
 # 2. Percabangan: langkah melompat jika kondisi terpenuhi
 # 3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 
+#Sekuensial
 print("Hello World!")
+print("Ibu berkata, \"Pergi ke Toko!\"")
+print("Budy menjawab, \"Baik, apa yang harus saya lakukan di toko?!\"")
+print("Ibu menjawab, \"Beli satu botol susu, dan jika ada telor beli beli 6?\"")
+print("maka budi berangkat ke toko")
+print("Dan mulai berbelanja")
