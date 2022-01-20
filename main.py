984ccc61b137ef1934fd385ae59a0af6d95f5308
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-20 17:17:35 +0700

    first hello world

---
+++ main.py
@@ -0,0 +1,6 @@
+# semua sintaksis dasar bahasa pemrograman terdiri dari:
+# 1. Sekuensial: langkah berurutan
+# 2. Percabangan: langkah melompat jika kondisi terpenuhi
+# 3. Perulangan: mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+
+print("Hello World!")
