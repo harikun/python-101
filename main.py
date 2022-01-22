4da5d2b4719f68328c70068372e7be33cb2c0e1f
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-22 22:38:10 +0700

    while loop

--- main.py
+++ main.py
@@ -34,14 +34,27 @@
 
 # for perulangan yang jelas berapa jumlah perulangannya
 # while tidak jelas berapa kali akan diulang tapi jelas kapan selesainya
+# perulangan dengan FOR LOOP
+# jumlah_buku = 10
+# print('ibu berkata, "Baca semua bukumu!"')
 
+# jumlah_buku_sudah_dibaca = 0
+# print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+
+# for jumlah_buku_sudah_dibaca in range(1, jumlah_buku+1):
+#  print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
+
+# print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+
+#perulangan dengan WHILE LOOP
 jumlah_buku = 10
 print('ibu berkata, "Baca semua bukumu!"')
 
 jumlah_buku_sudah_dibaca = 0
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
 
-for jumlah_buku_sudah_dibaca in range(1, jumlah_buku+1):
- print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
+while jumlah_buku_sudah_dibaca < jumlah_buku:
+  jumlah_buku_sudah_dibaca += 1
+  print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
 
 print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
