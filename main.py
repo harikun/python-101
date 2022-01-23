430fad242523874f68e5bd7b911898063900392e
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-23 19:05:52 +0700

    while loop 2

--- main.py
+++ main.py
@@ -47,14 +47,34 @@
 # print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
 
 #perulangan dengan WHILE LOOP
+# jumlah_buku = 10
+# print('ibu berkata, "Baca semua bukumu!"')
+
+# jumlah_buku_sudah_dibaca = 0
+# print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+
+# while jumlah_buku_sudah_dibaca < jumlah_buku:
+#   jumlah_buku_sudah_dibaca += 1
+#   print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
+
+# print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+
+#perulangan dengan WHILE LOOP sampai paham
 jumlah_buku = 10
 print('ibu berkata, "Baca semua bukumu!"')
+total_jumlah_baca = 0
+jumlah_buku_sudah_dibaca_dan_dipahami = 0
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
 
-jumlah_buku_sudah_dibaca = 0
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
-
-while jumlah_buku_sudah_dibaca < jumlah_buku:
-  jumlah_buku_sudah_dibaca += 1
-  print(f"Buku ke {jumlah_buku_sudah_dibaca} sudah dibaca")
-
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_sudah_dibaca}')
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_buku_sudah_dibaca_dan_dipahami == 9:
+      print(f"buku ke {jumlah_buku_sudah_dibaca_dan_dipahami + 1} belum paham")
+    else:
+      jumlah_buku_sudah_dibaca_dan_dipahami += 1
+      print(f"Buku ke {jumlah_buku_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+if jumlah_buku_sudah_dibaca_dan_dipahami == jumlah_buku:
+  print("Buku sudah selesai dibaca dan dipahami")
+else:
+  print(f"tidak semua buku bisa dipahami. Budi hanya bisa memahami {jumlah_buku_sudah_dibaca_dan_dipahami} buku")
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_sudah_dibaca_dan_dipahami}')
