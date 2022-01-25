df02d19928a42bb8bb110c68c5e6ae127ce0f3a1
Cipto Suhari <cipto.suheri@gmail.com>
2022-01-25 20:34:36 +0700

    project done

--- gempa.py
+++ gempa.py
@@ -20,18 +20,45 @@ def data_extraction():
     print("Tidak dapat mengambil data")
     return None
   if content.status_code == 200:
-    print(content.text)
-    # soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
-    # print(soup.prettify())
+    soup = BeautifulSoup(content.text, 'html.parser')
+    dateFull = soup.find('span', {'class': 'waktu'}).text
+    date = dateFull.split(', ')[1]
+    time = dateFull.split(', ')[0]
+    alldata = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+    alldata = alldata.findChildren('li')
+
+    magnitude = None
+    deep = None
+    ls = None
+    bt = None
+    epicenter = None
+    feel = None
+
+    i = 0
+    for data in alldata:
+      print(i, data)
+      if i == 1:
+        magnitude = data.text
+      elif i == 2:
+        deep = data.text
+      elif i == 3:
+        location = data.text.split(' - ')
+        ls = location[0]
+        bt = location[1]
+      elif i == 4:
+        epicenter = data.text
+      elif i == 5:
+        feel = data.text
+      i += 1
 
     hasil = dict()
-    hasil['date'] = '23 Januari 2022'
-    hasil['time'] = '12:20:13 WIB'
-    hasil['magnitude'] = 3.8
-    hasil['deep'] = '9 km'
-    hasil['location'] = {'LS':7.41, 'BT': 121.87}
-    hasil['epicenter'] = 'Pusat gempa berada di laut 139 km Barat Laut Maumere'
-    hasil['feel'] = 'Dirasakan (Skala MMI): II Pulau Kalaotoa'
+    hasil['date'] = date
+    hasil['time'] = time
+    hasil['magnitude'] = magnitude
+    hasil['deep'] = deep
+    hasil['location'] = {'LS':ls, 'BT': bt}
+    hasil['epicenter'] = epicenter
+    hasil['feel'] = feel
     return hasil
   else:
     print("Tidak dapat mengambil data")
@@ -47,7 +74,7 @@ def show_data(result):
   print(f"Tanggal: {result['date']}")
   print(f"Jam: {result['time']}")
   print(f"Magnitude: {result['magnitude']}")
-  print(f"Lokasi: {result['location']['LS']} LS {result['location']['BT']} BT")
+  print(f"Lokasi: {result['location']['LS']}, {result['location']['BT']}")
   print(f"Kedalaman: {result['deep']}")
   print(f"Epicentern: {result['epicenter']}")
   print(f"Dirasakan: {result['feel']}")
